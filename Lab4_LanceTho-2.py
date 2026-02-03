ff516c25ec4166ab39fb9eceefe9ede827fc4f3b
Lance Thongsavanh <147730520+LanceTho@users.noreply.github.com>
2026-02-03 14:08:38 -0500

    Create Lab4_LanceTho-2.py

---
+++ Lab4_LanceTho-2.py
@@ -0,0 +1,6 @@
+"""
+Lab4_LanceTho-2.py
+Lance Thongsavanh
+Write a program where the player tries to guess a random number between 1 and 15.  Each incorrect guess reveals another part of a hangman drawing.
+2/3/2026
+"""
